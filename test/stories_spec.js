73d917dc39b3e7c6533d5edc26a74ef61041349f
Piet Geursen <pietgeursen@gmail.com>
2015-05-16 16:40:50 +1200

    Add test for creating story

--- test/stories_spec.js
+++ test/stories_spec.js
@@ -7,7 +7,24 @@ var request = request(url);
 
 describe('Stories', function(){
 
-  describe('get /stories', function(){
+
+  describe('POST /stories', function(){
+
+  	it('gets success and returns json', function(done){
+  		var story = {story: {title: "Supernatural Winnipeg", contribution_limit: 12, lat:-41.270833, lng: 173.283889}}
+	  	request.post('/stories')
+	  		.set('Accept', 'application/json')
+	  		.send(story)
+	  		.expect('Content-Type', /json/)
+	  		.end(function(err, res){
+	  			console.log(res.body.story.title);
+				(res.body.story.title).should.be.eql(story.story.title)	  			
+	  			done()
+	  		});  		
+  	})
+  })
+
+  describe('GET /stories', function(){
 
   	it('gets success and returns json', function(done){
 	  	request.get('/stories')
