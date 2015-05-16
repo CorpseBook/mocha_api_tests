0db7cd33680e9de0a425ecaeb1e56103b3f7784a
Piet Geursen <pietgeursen@gmail.com>
2015-05-16 16:04:12 +1200

    Init basic test file structure.

---
+++ test/contributions_spec.js
@@ -0,0 +1,8 @@
+var should = require("should")
+describe('Array', function(){
+  describe('#numbers', function(){
+    it('5 ===5 ', function(){
+    	(5).should.be.exactly(5).and.be.a.Number;
+    })
+  })
+})
